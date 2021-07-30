3c45302e1c2a09746e0b3b990973ebe7570f886b
boks01 <msal40755@gmail.com>
2021-07-30 17:19:50 +0700

    adding new file again guyz

---
+++ pemrograman_dasar_x_mm/pascal_0_training/main.pas
@@ -0,0 +1,22 @@
+program calculator;
+var x:integer;
+var op:string;
+var y:integer;
+procedure addition(x, y);
+ writeln('the result is',x+y);
+ readln;
+begin
+procedure subtraction(x, y);
+ writeln('the result is',x-y);
+ readln;
+begin
+    
+end;
+begin
+    write('num 1 : ');
+    readln(x);
+    write('operation (+, -) : ');
+    readln(x);
+    write('num 2 : ');
+    readln(y);
+end;
